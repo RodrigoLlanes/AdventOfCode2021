13f5abcfdfe465db1ab81e9765aa9f234016f755
RodrigoLlanes <romadritex@gmail.com>
2021-12-21 06:48:51 +0100

    Day21-without readme

--- Day21/Sol2.py
+++ Day21/Sol2.py
@@ -2,26 +2,20 @@ import re
 from functools import cache
 
 
-def roll_dice(dice):
-    res = 0
-    for _ in range(3):
-        if dice == 101:
-            dice = 1
-        res += dice
-        dice += 1
-    return res
+def roll_dice():
+    return [a + b + c for a in range(1, 4) for b in range(1, 4) for c in range(1, 4)]
 
 
 @cache
 def play(p0, p1, s0, s1):
     res = [0, 0]
-    for i in [a + b + c for a in range(1, 4) for b in range(1, 4) for c in range(1, 4)]:
+    for i in roll_dice():
         np0 = ((p0 - 1 + i) % 10) + 1
         ns0 = s0 + np0
         if ns0 >= 21:
             res[0] += 1
         else:
-            for j in [a + b + c for a in range(1, 4) for b in range(1, 4) for c in range(1, 4)]:
+            for j in roll_dice():
                 np1 = ((p1 - 1 + j) % 10) + 1
                 ns1 = s1 + np1
                 if ns1 >= 21:
